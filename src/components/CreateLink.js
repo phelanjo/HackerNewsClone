d8f9167ceb404a06bbbe172ff0cee8261e397558
Johnny Phelan <phelanjo@oregonstate.edu>
2019-11-07 14:28:20 -0800

    Use mutation component passing POST_MUTATION as prop.

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -37,7 +37,13 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <button onClick={`... you'll implement this 🔜`}>Submit</button>
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+          {() => (
+            <button onClick={`... you'll implement this 🔜`}>
+              Submit
+              </button>
+          )}
+        </Mutation>
       </div>
     )
   }
